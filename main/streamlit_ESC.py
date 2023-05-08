d2b137d8d3a05404f2c7bfb03320374eb2dbbf95
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2023-05-08 22:02:53 +0200

    Update streamlit_ESC.py

--- main/streamlit_ESC.py
+++ main/streamlit_ESC.py
@@ -884,7 +884,7 @@ elif app_mode == '🤖 Predicción Eurovisión 2023':
     st.markdown('<h1 style="text-align:center"><span style="font-size: 40px;">📈</span> <u>PREDICCIONES 30 DÍAS ANTES</u></h1>', unsafe_allow_html=True)
     st.write('')
     st.write('')
-    st.success('En este apartado podrás realizar una predicción en vivo de las canciones participantes en el Festival de Eurovisión del sábado 13 de mayo de 2023. Visualizarás la predicción en fecha y hora actual y un gráfico con la evolución de dicha predicción a lo largo de los 30 días previos al concurso.')
+    st.success('En este apartado podrás realizar una predicción en vivo de las canciones participantes en el Festival de Eurovisión del sábado 13 de mayo de 2023. Visualizarás la estimación en fecha y hora actual y un gráfico con la evolución de ésta a lo largo de los 30 días previos al concurso.')
     
     # CARGAMOS DATA TO TRAIN
     data = pd.read_excel("./data/Data_to_train.xlsx")
