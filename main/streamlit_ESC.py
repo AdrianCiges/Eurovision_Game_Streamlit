5edf180973d0e02fe0a4928bb37cfbd432cf4775
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2024-04-08 12:06:17 +0200

    Update streamlit_ESC.py

--- main/streamlit_ESC.py
+++ main/streamlit_ESC.py
@@ -1503,7 +1503,6 @@ elif app_mode == '📊 Estadísticas 2002-2023':
 
     # Muestra el DataFrame filtrado
     st.write('\n')
-    st.write('\n')
 
     st.markdown("<h4 style='margin-bottom: 5px;'>Tabla de datos</h4>", unsafe_allow_html=True)
     with st.expander('👀 Ver Datos', expanded=False): 
