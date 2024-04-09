0ed7f6bd7c86318a2a0b52d70a076ae5e78373a3
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2024-04-09 13:36:32 +0200

    Update streamlit_ESC.py

--- main/streamlit_ESC.py
+++ main/streamlit_ESC.py
@@ -2314,7 +2314,7 @@ with tab2:
         st.write(cols_in_range)
         
         # Filtrar el DataFrame para incluir solo las columnas dentro del rango de años
-        df_histo = df_histo[['country', 'Image URL'] + cols_in_range[0:2]]
+        df_histo = df_histo[['country', 'Image URL' + cols_in_range]
         
         # Si también necesitas filtrar por países seleccionados:
         if selected_country:
