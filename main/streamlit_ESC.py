810192fdc6a5a5ecbd175e1fc0dc776c682a60b6
Adrián C. Ciges <109532909+AdrianCiges@users.noreply.github.com>
2024-04-10 12:31:35 +0200

    Update streamlit_ESC.py

--- main/streamlit_ESC.py
+++ main/streamlit_ESC.py
@@ -767,7 +767,7 @@ def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
                         min_value=_min,
                         max_value=_max,
                         value=(_min, _max),
-                        step=1,
+                        step=1.00,
                     )
                     st.write('-----------')
                     df = df[df[column].between(*user_num_input)]
